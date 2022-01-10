3ce3f87a32f86a530d3b3f8dde4e1fb14f3b443a
sym <sym@humuson.com>
2022-01-10 22:12:40 +0900

    Initial commit

---
+++ src/test/java/com/humuson/springapiproject/SpringApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humuson.springapiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
